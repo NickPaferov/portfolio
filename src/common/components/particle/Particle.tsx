0dae5a1175f467b8394f1a56975714971e2b56d1
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-12-04 19:28:48 +0300

    portfolio (form refactoring in Contacts)

--- src/common/components/particle/Particle.tsx
+++ src/common/components/particle/Particle.tsx
@@ -26,7 +26,7 @@ export const Particle = () => {
                 interactivity: {
                     events: {
                         onClick: {
-                            enable: true,
+                            enable: false,
                             mode: "push",
                         },
                         onHover: {
--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,34 +2,72 @@ import React, {useState} from 'react';
 import style from './Contacts.module.scss'
 import Title from "../common/components/title/Title";
 import {Fade} from 'react-awesome-reveal';
+import {useFormik} from 'formik';
+import axios from 'axios';
 
 const Contacts = () => {
 
-    const [offer, setOffer] = useState(false)
+    const [notification, setNotification] = useState<null | string>(null)
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+        onSubmit: async ({name, email, message}) => {
+            setNotification("Letter is sending...")
+            setIsDisabled(true)
+            try {
+                await axios.post("http://localhost:3010/sendMessage", {name, email, message})
+                setNotification("Your message has been sent. I will answer you when I find time.")
+                formik.resetForm()
+            } catch (error: any) {
+                setNotification(`${error.message}. Try later.`)
+            } finally {
+                setIsDisabled(false)
+            }
+        },
+    })
+
+    const noticeBtn = isDisabled ? style.disabledBtn : style.activeBtn
 
     return (
         <div id={"contacts"} className={style.contactsBlock}>
             <div className={style.contactsContainer}>
                 <Title text={"Contacts"}/>
                 <Fade direction={"down"} className={style.fadeWrapper}>
-                    {offer
-                        ? <div className={style.offer}>
-                            <div className={style.notification}><span>Your message has been sent</span>
-                                <br/>
-                                <span>I will answer you when I find time</span>
-                            </div>
-                            <button onClick={() => setOffer(false)}>Ok</button>
-                        </div>
-                        : <form className={style.contactForm}>
-                            <input className={style.input} type={"text"} placeholder={"Name"}/>
-                            <input className={style.input} type={"email"} placeholder={"E-mail"}/>
-                            <textarea className={style.textarea} placeholder={"Your message"}/>
-                            <button type={"submit"}
-                                    onClick={(e) => {
-                                        e.preventDefault()
-                                        setOffer(true)
-                                    }}>Send message
+                    {notification
+                        ? <div className={style.notificationForm}>
+                            <div className={style.notification}>{notification}</div>
+                            <button className={noticeBtn}
+                                    onClick={() => setNotification(null)}
+                                    disabled={isDisabled}>Ok
                             </button>
+                        </div>
+                        : <form className={style.contactForm} onSubmit={formik.handleSubmit}>
+                            <input className={style.input}
+                                   id="name"
+                                   name="name"
+                                   type={"text"}
+                                   placeholder={"Name"}
+                                   onChange={formik.handleChange}
+                                   value={formik.values.name}/>
+                            <input className={style.input}
+                                   id="email"
+                                   name="email"
+                                   type={"email"}
+                                   placeholder={"E-mail"}
+                                   onChange={formik.handleChange}
+                                   value={formik.values.email}/>
+                            <textarea className={style.textarea}
+                                      id="message"
+                                      name="message"
+                                      placeholder={"Your message"}
+                                      onChange={formik.handleChange}
+                                      value={formik.values.message}/>
+                            <button type={"submit"}>Send message</button>
                         </form>}
                 </Fade>
             </div>
