68d4fa8064dcdf0120a5aa8b530700cc776ec8bd
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-01-26 22:54:18 +0300

    portfolio (refactoring of Footer component with styles)

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -6,38 +6,61 @@ import gitHubImage from './../assets/images/github.png'
 import telegramImage from './../assets/images/telegram.jpg'
 import Social from "./social/Social";
 import Title from '../common/components/title/Title';
-import { Fade } from 'react-awesome-reveal';
+import {Fade} from 'react-awesome-reveal';
+import {v1} from 'uuid';
 
 const Footer = () => {
-    const codewarsStyle = {
-        backgroundImage: `url(${codeWarsImage})`
-    }
-    const freeCodeCampStyle = {
-        backgroundImage: `url(${freeCodeCampImage})`
-    }
-    const gitHubStyle = {
-        backgroundImage: `url(${gitHubImage})`
-    }
-    const telegramStyle = {
-        backgroundImage: `url(${telegramImage})`
-    }
+
+    const socials = [
+        {
+            id: v1(),
+            platform: "Codewars",
+            link: "https://www.codewars.com/users/NickPaferov",
+            style: {
+                backgroundImage: `url(${codeWarsImage})`
+            }
+        },
+        {
+            id: v1(),
+            platform: "FCC",
+            link: "https://www.freecodecamp.org/NickPaferov",
+            style: {
+                backgroundImage: `url(${freeCodeCampImage})`
+            }
+        },
+        {
+            id: v1(),
+            platform: "GitHub",
+            link: "https://github.com/NickPaferov",
+            style: {
+                backgroundImage: `url(${gitHubImage})`
+            }
+        },
+        {
+            id: v1(),
+            platform: "Telegram",
+            link: "https://t.me/NickPaferov",
+            style: {
+                backgroundImage: `url(${telegramImage})`
+            }
+        },
+
+    ]
 
     return (
         <div className={style.footer}>
             <div className={style.footerContainer}>
                 <Title text={"Nikolai Paferov"}/>
-                    <div className={style.socialIcons}>
-                <Fade direction={"down"}>
-                        <Social style={codewarsStyle} platform={"Codewars"}
-                                link={"https://www.codewars.com/users/NickPaferov"}/>
-                        <Social style={freeCodeCampStyle} platform={"FCC"}
-                                link={"https://www.freecodecamp.org/NickPaferov"}/>
-                        <Social style={gitHubStyle} platform={"GitHub"}
-                                link={"https://github.com/NickPaferov"}/>
-                        <Social style={telegramStyle} platform={"Telegram"}
-                                link={"https://t.me/NickPaferov"}/>
-                </Fade>
-                    </div>
+                <div className={style.socialIcons}>
+                    <Fade direction={"down"}>
+                        {socials.map(social =>
+                            <Social key={social.id}
+                                    style={social.style}
+                                    platform={social.platform}
+                                    link={social.link}/>
+                        )}
+                    </Fade>
+                </div>
                 <span className={style.copyRight}>© 2022 All rights reserved</span>
             </div>
         </div>
